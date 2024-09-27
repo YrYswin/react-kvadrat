817d2938f07d85950b09a79f4bffdfc94a38bd71
Abdurahman <abdurahman@Noutbuk-Abdurahman.local>
2024-09-27 12:35:16 +0600

    fix: header

--- src/widgets/Header/Modal.tsx
+++ src/widgets/Header/Modal.tsx
@@ -10,6 +10,7 @@ interface Props {
 
 const style = "w-full pl-[20px] py-[12px] cursor-pointer hover:bg-[#292929] hover:text-white group";
 const hr = "h-px rounded-full ml-[20px] bg-gray-200 border-0 dark:bg-gray-400 transition-opacity group-hover:hidden";
+
 const Modal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
   const scrollToSection = React.useContext(ScrollContext);
   const navigate = useNavigate();
@@ -26,13 +27,19 @@ const Modal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
     }, 100);
   };
 
+  const stopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation(); // Предотвращает закрытие при клике на содержимое модального окна
+  };
+
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div onClick={() => setIsOpen(false)} className="absolute inset-0 bg-black bg-opacity-50 z-[99] h-full mt-[65px]">
-      <div className="w-[219px] h-[380px] bg-red-700 text-md text-white absolute rounded-l-b-md rounded-bl-3xl right-0 py-[19px]  z-[100]">
+    <div onClick={() => setIsOpen(false)} className="fixed inset-0 z-[99] h-full bg-black bg-opacity-50 mt-[65px]">
+      <div
+        onClick={stopPropagation}
+        className="w-[219px] h-[380px] bg-red-700 text-md text-white absolute rounded-l-b-md rounded-bl-3xl right-0 py-[19px] z-[100]">
         <div className="flex flex-col">
           <Link to="/">
             <p onClick={() => setIsOpen(!isOpen)} className={style}>
